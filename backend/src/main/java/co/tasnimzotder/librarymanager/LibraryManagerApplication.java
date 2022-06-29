67c0f6a3a52295df4f4cce365f02d5ad6d214d49
tasnimzotder <tasnimzotder@gmail.com>
2022-06-29 16:20:23 +0530

    init backend

---
+++ backend/src/main/java/co/tasnimzotder/librarymanager/LibraryManagerApplication.java
@@ -0,0 +1,13 @@
+package co.tasnimzotder.librarymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagerApplication.class, args);
+	}
+
+}
